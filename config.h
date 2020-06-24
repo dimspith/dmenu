16be4717ba598b707fa25a4f987ba8e98fffd315
dimitrissp <dimitrissp@posteo.gr>
2020-06-24 03:29:18 +0300

    Change colors and fonts

--- config.h
+++ config.h
@@ -4,17 +4,19 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
+    "CaskaydiaCove Nerd Font:size=10",
+	"Mononoki:size=10",
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                   fg         bg    */
-	[SchemeNorm] = { "#ebdbb2", "#1a1a24" },
-	[SchemeSel] =  { "#ebdbb2", "#3F3662" },
+	[SchemeNorm] = { "#dfdfaf", "#080808" },
+	[SchemeSel] =  { "#dfdfaf", "#662727" },
 	[SchemeOut] =  { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 5;
 
 /*
  * Characters not considered part of a word while deleting words
